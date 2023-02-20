ac106dc706adadd200afd2b7ee6eead21a551c5d
ailoher <aitor.lopez.hernandez@gmail.com>
2023-02-20 17:15:39 +0100

    Initial commit to define the project skeleton

---
+++ ball-ai/court_objects/court.py
@@ -0,0 +1,6 @@
+class TennisCourt:
+    def __init__(self, length=23.77, width=8.23, service_line=6.40, net_height=0.91):
+        self.length = length
+        self.width = width
+        self.service_line = service_line
+        self.net_height = net_height
